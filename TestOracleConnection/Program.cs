2f0ddd3bd8812708e956756ae74b0e146963a175
343588387 <343588387@qq.com>
2017-05-04 10:42:56 +0800

    code

--- TestOracleConnection/Program.cs
+++ TestOracleConnection/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 using Microsoft.Practices.EnterpriseLibrary.Data;
 
@@ -10,8 +11,14 @@ namespace TestOracleConnection
         //需要测试该代码，只要改动sql语句和连接数据库字符串即可。
         static void Main(string[] args)
         {
-            object result = OracleDataProvider.TestReadTable("110");
+            //object result = OracleDataProvider.TestReadTable("110");
+            DataTable result = OracleDataProvider.TestReadTable("110");
+            for (int i = 0; i < result.Rows.Count; i++)
+            {
+                Console.WriteLine(result.Rows[i]["BOOKNAME"].ToString());
+            }
 
+            Console.Read();
             Console.WriteLine();
         }
     }
