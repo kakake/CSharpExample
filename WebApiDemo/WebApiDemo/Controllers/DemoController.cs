04514b4492b03760fd230c660a565cff34115116
343588387 <343588387@qq.com>
2017-05-05 15:40:01 +0800

    code

---
+++ WebApiDemo/WebApiDemo/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    public class DemoController : ApiController
+    {
+        [HttpGet]
+        public IList<Site> SiteList(int startId, int itemcount)
+        {
+            var sites = new List<Site>();
+            sites.Add(new Site { SiteId = 1, Title = "test", Uri = "www.cnblogs.cc" });
+            sites.Add(new Site { SiteId = 2, Title = "博客园首页", Uri = "www.cnblogs.com" });
+            sites.Add(new Site { SiteId = 3, Title = "博问", Uri = "q.cnblogs.com" });
+            sites.Add(new Site { SiteId = 4, Title = "新闻", Uri = "news.cnblogs.com" });
+            sites.Add(new Site { SiteId = 5, Title = "招聘", Uri = "job.cnblogs.com" });
+
+            var result = (from Site site in sites
+                          where site.SiteId > startId
+                          select site)
+                            .Take(itemcount)
+                            .ToList();
+            return result;
+        }
+    }
+}
---
+++ WebApiDemo/WebApiDemo/Models/Site.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiDemo.Models
+{
+    public class Site
+    {
+        public int SiteId { get; set; }
+        public string Title { get; set; }
+        public string Uri { get; set; }
+    }
+}
